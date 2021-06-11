479ee49d40690119adcb43e4b87fcc2c61ed48bc
Maxim Savchenko <maxim.savchenko@wdg.com.ua>
2021-06-11 03:04:40 +0300

    add update layout over desktop version and add some js

--- src/assets/js/jQuery.js
+++ src/assets/js/jQuery.js
@@ -1,9 +1,7 @@
 $(function () {
     function tabsEfect() {
         let item = $('.tabs__item');
-            console.log(item);
-            
-        
+    
         item.on('click' , function () {
             item.removeClass('active');
             $(this).addClass('active');
@@ -11,4 +9,28 @@ $(function () {
     }
 
     tabsEfect()
+
+    function profileDropdown() {
+        let dropDownBtn = $('.profile__btn'),
+            dropDownList = $('.profile__dropdown-list');
+
+            dropDownBtn.on('click' , function () {
+                dropDownList.fadeToggle(200)
+            })
+
+            $(document).on("click", function (e) {
+                if (
+                    !dropDownBtn.is(e.target) &&
+                    dropDownBtn.has(e.target).length === 0 &&
+                    !dropDownList.is(e.target) &&
+                    dropDownList.has(e.target).length === 0
+                ) {
+                  dropDownList.fadeOut(200);
+                }
+            });
+    }
+
+    profileDropdown();
+
+    
 });
